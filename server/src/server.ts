6a86ad32dcfc154bbc974fd06bb9375c03f23b14
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-09-13 23:51:07 -0600

    Update static file service location

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("../../client/dist"));
+app.use(express.static("../../../client/dist"));
 
 // TODO: Implement middleware to connect the routes
 app.use(routes);
