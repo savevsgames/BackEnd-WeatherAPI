cd087bb81b8c53e087367390fa1748bb058655d0
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-09-14 00:08:50 -0600

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "./" : "/", // Ensure proper base path for production
+    server: {
+      port: 3000,
+      open: true,
+      proxy: {
+        "/api": {
+          target: "http://localhost:3001",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  build: {
-    outDir: "dist",
-  },
+    build: {
+      outDir: "dist",
+    },
+  };
 });
