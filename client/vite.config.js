9994fc29a12b60673e6e0b4374a36e88ec148115
savevsgames <126988787+savevsgames@users.noreply.github.com>
2024-09-14 02:21:48 -0600

    Still trying to resolve path issues when deploying

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,16 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => {
-  return {
-    base: command === "build" ? "./" : "/", // Ensure proper base path for production
-    server: {
-      port: 3000,
-      open: true,
-      proxy: {
-        "/api": {
-          target: "http://localhost:3001",
-          changeOrigin: true,
-          secure: false,
-        },
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+        secure: false,
       },
     },
-    build: {
-      outDir: "dist",
-    },
-  };
+  },
 });
